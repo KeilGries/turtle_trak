ba9495f47d3d28729060953d081bcc2c50e36d0f
KG <KeilGriesLP@gmail.com>
2023-06-13 15:10:00 -0400

    Reorganized code and linked bottom nav icons for 'Enclosure Climate' + 'Growth' to the relevant page widget. Added more images.

--- turtle_trak/lib/main.dart
+++ turtle_trak/lib/main.dart
@@ -11,63 +11,136 @@ class TurtleTrak extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: MainPage(),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Center(child: Text('TurtleTrak')),
-        backgroundColor: Color.fromARGB(255, 0, 126, 0),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.thermostat),
-            label: 'Enclosure Climate',
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.ruler),
-            label: 'Growth',
-          ),
-        ],
-      ),
-      body: Image(
-        image: AssetImage('images/TurtleTrak.jpeg'),
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+  final screens = [
+    HomePage(),
+    Center(
+      child: Image(
+          image: AssetImage('images/mock_climate_page.png'),
+          width: 400.0,
+          height: 800.0,
+          fit: BoxFit.fill),
+    ),
+    Center(
+      child: Image(
+        image: AssetImage('images/mock_growth_chart.png'),
         width: 400.0,
         height: 800.0,
         fit: BoxFit.fill,
       ),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Center(child: Text('TurtleTrak')),
+          backgroundColor: Color.fromARGB(255, 0, 126, 0),
+        ),
+        body: screens[currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color.fromARGB(255, 0, 126, 0),
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white54,
+          showUnselectedLabels: false,
+          iconSize: 27,
+          currentIndex: currentIndex,
+          onTap: (index) => setState(() => currentIndex = index),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.thermostat),
+              label: 'Enclosure Climate',
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.ruler),
+              label: 'Growth',
+            ),
+          ],
+        ));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: const [
+          Image(
+            image: AssetImage('images/TurtleTrak.jpeg'),
+            width: 400.0,
+            height: 800.0,
+            fit: BoxFit.fill,
+          ),
+          Positioned(
+            top: 0,
+            bottom: 110,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Turtle',
+                  style: TextStyle(
+                    fontSize: 70,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 250, 248, 248),
+                  ),
+                ),
+                Text(
+                  'Trak',
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontStyle: FontStyle.italic,
+                    color: Color.fromARGB(255, 0, 126, 0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-class GrowthScreen extends StatelessWidget {
-  const GrowthScreen({super.key});
+class ClimatePage extends StatelessWidget {
+  const ClimatePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Placeholder();
   }
 }
 
-class ClimateScreen extends StatelessWidget {
-  const ClimateScreen({super.key});
+class GrowthPage extends StatelessWidget {
+  const GrowthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    return const Placeholder();
   }
 }
